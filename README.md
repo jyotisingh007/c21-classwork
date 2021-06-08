e52c9c802321a909287236db3a06c13418ef5880
Jyoti Singh <75181587+jyotisingh007@users.noreply.github.com>
2021-06-08 20:31:12 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c21-classwork
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+
+  //console.log(movingRect.x);
+  //console.log(movingRect.x - fixedRect.x);
+
+ console.log(movingRect.y- fixedRect.y);
+
+  //isTouching();
+
+  drawSprites();
+}
+
+/*function isTouching(){
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2) {
+  movingRect.shapeColor = "red";
+  fixedRect.shapeColor = "red";
+}
+else {
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green";
+}
+}*/
